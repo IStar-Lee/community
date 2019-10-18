c455c44145d74f534b7bba8aac88392b2554e717
IStar_Lee <IStar_Lee@126.com>
2019-10-18 19:36:19 +0800

    commit code with modify info from database that when you login again auto

--- src/main/java/life/mastar/community/community/dto/GithubUser.java
+++ src/main/java/life/mastar/community/community/dto/GithubUser.java
@@ -13,6 +13,10 @@ public class GithubUser {
      * 用户github的bio，即描述
      */
     private String bio;
+    /**
+     * 头像
+     */
+    private String avatar_url;
 
     public String getName() {
         return name;
@@ -36,5 +40,13 @@ public class GithubUser {
 
     public void setBio(String bio) {
         this.bio = bio;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
     }
 }
--- src/main/java/life/mastar/community/community/mapper/UserMapper.java
+++ src/main/java/life/mastar/community/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.mastar.community.community.mapper;
 import life.mastar.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,4 +15,6 @@ import org.springframework.stereotype.Component;
 public interface UserMapper {
     @Insert("insert into user(account_id,name,token,gmt_create,gmt_modify) values (#{accountId},#{name},#{token},#{gmtCreate},#{gmtModify})")
     public void insert(User user);
+    @Select("select * from user where token = #{token}")
+    public User findByToken(@Param("token") String token);
 }
