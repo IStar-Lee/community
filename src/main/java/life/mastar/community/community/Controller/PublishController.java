6f48bd9ac47d627962aac7e65bf528342ae4b352
IStar_Lee <IStar_Lee@126.com>
2019-10-29 18:58:02 +0800

    commit code with add publish.html

---
+++ src/main/java/life/mastar/community/community/Controller/PublishController.java
@@ -0,0 +1,12 @@
+package life.mastar.community.community.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PublishController {
+    @RequestMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+}
