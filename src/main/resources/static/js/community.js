ec718a598127902e625dc4edfffbb0566c8dd818
lgx <IStar_Lee@126.com>
2020-02-16 15:47:55 +0800

    commit code with repair function to login without refresh

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -42,7 +42,31 @@ function post() {
             if(response.code == 200){
                 $("#comment-section").hide();
             }else{
-                alert(response.message);
+                if(response.code == 2001){//解决没有登录直接回复问题
+                    top.swal({
+                            title: "登录",
+                            text: "您当前没有登录，必须登录后回复，是否现在登录？",
+                            type: "warning",
+                            showCancelButton: true,
+                            confirmButtonColor: "#DD6B55",
+                            confirmButtonText: "马上登录",
+                            cancelButtonText: "让我再考虑一下…",
+                            closeOnConfirm: false,
+                            closeOnCancel: false,
+                            customClass: "custom_swal"
+                        },
+                        function (isConfirm) {
+                            if(isConfirm){
+                                window.open("https://github.com/login/oauth/authorize?client_id=c53ac12fc34d039e1fa7&redirect_uri=http://localhost:8080/callback&scope=user&state=1")
+                                window.localStorage.setItem("param",true);
+                                top.swal.close()
+                            }else{
+                                top.swal.close()
+                            }
+                        });
+                }else{
+                    alert(response.message);
+                }
             }
         },
         dataType: "json"
